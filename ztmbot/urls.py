4d2c5810c360b593e14cd166c649cec35e9d0756
WD Snoeijer <88512452+dsnoeijer@users.noreply.github.com>
2022-04-12 22:20:50 +0200

    adding scoreboard

--- ztmbot/urls.py
+++ ztmbot/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from .quiz.views import RandomQuestion
+from .score.views import UpdateScores
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/random/', RandomQuestion.as_view(), name="random")
+    path('api/random/', RandomQuestion.as_view(), name='random'),
+    path('api/score/update/', UpdateScores.as_view(), name='score_update'),
 ]
