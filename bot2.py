73291e60fd07315d91eb75623fe41ce0f21e0571
WD Snoeijer <88512452+dsnoeijer@users.noreply.github.com>
2022-04-17 16:20:40 +0200

    bot2.py test

---
+++ bot2.py
@@ -0,0 +1,21 @@
+import os
+import discord
+from discord.ext import commands
+from discord_slash import SlashCommand, SlashContext
+
+
+CLIENT = discord.Client()
+TOKEN = os.environ.get('BOT_TOKEN')
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+slash = SlashCommand(bot)
+
+
+@slash.slash(name="test")
+async def _test(ctx: SlashContext):
+    embed = discord.Embed(title="embed test")
+    await ctx.send(content="test", embeds=[embed])
+
+bot.run("discord_token")
+
+
+CLIENT.run(TOKEN)
